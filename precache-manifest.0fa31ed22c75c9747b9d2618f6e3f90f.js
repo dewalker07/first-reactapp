d7c80b5ff670419f352d84b0a8ff3948ced00216
Diwakar <dabbara.divakar@gmail.com>
2019-05-06 18:44:35 +0530

    Updates

---
+++ precache-manifest.0fa31ed22c75c9747b9d2618f6e3f90f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fedd88b88e43c559ce38e085a3a1abe",
+    "url": "/first-app/index.html"
+  },
+  {
+    "revision": "f6dff1933f7310e12f00",
+    "url": "/first-app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/first-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f6dff1933f7310e12f00",
+    "url": "/first-app/static/js/main.21686614.chunk.js"
+  },
+  {
+    "revision": "9bc2a1921ac1eff84df1",
+    "url": "/first-app/static/js/runtime~main.ada60267.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
